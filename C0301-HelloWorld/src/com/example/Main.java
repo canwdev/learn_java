905f8045da1ef97dc6e0ff0c8e9b55ef93428e1d
canw <c2097@live.com>
2017-10-08 20:21:44 +0800

    Re-HelloWorld

--- C0301-HelloWorld/src/com/example/Main.java
+++ C0301-HelloWorld/src/com/example/Main.java
@@ -3,7 +3,8 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world  \u2122");
+        String all = String.join(" · ", "H", "E", "L", "L", "O");
+        // Java 8 API [Document](https://docs.oracle.com/javase/8/docs/api/index.html)
+        System.out.println("Hello world\u2122\n" + all);
     }
 }
