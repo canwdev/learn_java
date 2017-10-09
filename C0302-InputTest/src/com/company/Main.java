1402332902cbc0076a4ac34eeff8d296a1750fa3
canw <c2097@live.com>
2017-10-09 20:03:59 +0800

    Input stream

--- C0302-InputTest/src/com/company/Main.java
+++ C0302-InputTest/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +13,9 @@ public class Main {
                 "we assure not to make your data public\n" +
                 "and won't save the data on any web server.\n" +
                 "Do you agree? (y/n)");
-        // TODO: 2017/10/8 in.nextChar??
+
         String agreement = in.nextLine();
+        // String ch = String.valueOf(agreement.charAt(0));
         if (!agreement.equalsIgnoreCase("y")) {
             System.exit(0);
         }
@@ -23,6 +26,21 @@ public class Main {
         System.out.println("How old are you?");
         int age = in.nextInt();
 
-        System.out.println("Welcome, " + name + ". Next year, you'll be " + (age + 1)           );
+        System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
+        System.out.printf("Hello, %s. Next year, you'll be %d\n", name, age + 1);
+        String formatResult = String.format("Hello, %s. Next year, you'll be %d\n"
+                , name, age + 1);
+        System.out.println(formatResult);
+        {
+            in.nextLine();
+            System.out.println("Press any key to continue...");
+            in.nextLine();
+        }
+
+        System.out.println("Welcome to our system, in this system, you can look time.");
+        System.out.println(new Date());
+        System.out.printf("%tc\n",new Date());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(new Date()));
     }
 }
